0ed1ab89604b7184cd10347b8a84d52cd69da891
Gon2153 <125921214+Gon21513@users.noreply.github.com>
2023-10-17 16:46:22 -0600

    Un led encendido
    Se enciende uno de los led

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 //Luis Pedro Gonzalez 21513
 //Lab7ED2
+//blinky example
 
 
 
@@ -12,8 +13,24 @@
 
 #define XTAL 16000000 //reloj externo de 16MHz
 
+// Definiciones para los LEDS
+#define RED_LED   GPIO_PIN_1
+#define BLUE_LED  GPIO_PIN_2
+#define GREEN_LED GPIO_PIN_3
+
+//Variables
+volatile uint32_t ui32Loop; //variable para el delay
+
 
 void setup(void); //Función del setup
+
+
+
+//delay para los LEDS
+
+void delay(void) {
+    for(ui32Loop = 0; ui32Loop < 2000000; ui32Loop++) {} // Simple delay
+}
 
 
 void setup(void){
@@ -24,6 +41,11 @@ void setup(void){
     //uitliza el oscilaro principal para el reloj
     SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL |SYSCTL_XTAL_16MHZ| SYSCTL_OSC_MAIN);
 
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);//habilita gpio para los leds
+
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED);// definir leds como salidas
+
 }
 
 int main(void){
@@ -33,8 +55,19 @@ int main(void){
 //loop principal
 while(1) {
 
-    //hola
+
+    //empezar a encender los leds
+
+    //enciende led rojo
+    //porf es para el puerto f
+    //pines quev voy a usar
+    //pin a enceder, en esre caso el rojos
+
+    GPIOPinWrite(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED, RED_LED);
+    delay();
+
 }
+
 
 
 }
