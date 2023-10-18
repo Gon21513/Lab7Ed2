e46d2fe352bc90eaa8248656d8e4c9ed6c4e46be
Gon2153 <125921214+Gon21513@users.noreply.github.com>
2023-10-17 22:53:30 -0600

    Semaforo
    Se implemtó logica con el boton para iniciar el semaforo

--- main.c
+++ main.c
@@ -13,32 +13,27 @@
 #include "inc/hw_types.h"
 #include "driverlib/systick.h" //libreria para el segundo delay
 
-
-
 #define XTAL 16000000 //reloj externo de 16MHz
 
 // Definiciones para los LEDS
 #define RED_LED   GPIO_PIN_1
 #define BLUE_LED  GPIO_PIN_2
 #define GREEN_LED GPIO_PIN_3
+#define BUTTON_PIN GPIO_PIN_4 //pin para el boton SW1
 
 //Variables
 volatile uint32_t ui32Loop; //variable para el delay
 volatile bool delayComplete = false; // Usado para indicar cuando el SysTick ha completado el delay
-
+int estado = 0; //bandera para el estado de las leds
 
 void setup(void); //Función del setup
-
-
 
 //funciones
 
 //segundo delay2
-
 void SysTickHandler(void) {
     delayComplete = true; // Indica que el delay ha sido completado
 }
-
 
 //delay2
 void delay2(void) {
@@ -48,72 +43,66 @@ void delay2(void) {
     }
 }
 
-
-//delay para los LEDS
-
-void delay(void) {
-    for(ui32Loop = 0; ui32Loop < 2000000; ui32Loop++) {} // Simple delay
+//chequear si el boton se presiona
+bool pressed(void) {
+    return GPIOPinRead(GPIO_PORTF_BASE, BUTTON_PIN) == 0; //true 0 si se presiona, false  1 si no
 }
 
+//delay para los LEDS
+void delay(void) {
+    for(ui32Loop = 0; ui32Loop < 3000000; ui32Loop++) {} // Simple delay
+}
 
 void setup(void){
-
-    //Define que PLL se divide en 4
-    //Se lockea hasta que hasta la nueva configuracion
-    //Frecuanci adel osciladro de 16mhz
-    //uitliza el oscilaro principal para el reloj
+    // Define que PLL se divide en 4
+    // Se lockea hasta que hasta la nueva configuracion
+    // Frecuanci adel osciladro de 16mhz
+    // uitliza el oscilaro principal para el reloj
     SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL |SYSCTL_XTAL_16MHZ| SYSCTL_OSC_MAIN);
-
-
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);//habilita gpio para los leds
-
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED);// definir leds como salidas
-
-
-//otra forma de realizar el delay
-
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); //habilita gpio para los leds
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)); //Esperar a que se inicialice
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED); // definir leds como salidas
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, BUTTON_PIN); //configurar el pin como entrada para el boton SW1
+    GPIOPadConfigSet(GPIO_PORTF_BASE, BUTTON_PIN, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
     // Configurar el SysTick para 1 segundo
     SysTickPeriodSet(SysCtlClockGet());
     SysTickIntRegister(&SysTickHandler); // Registrar la función del manejador
     SysTickIntEnable();
     SysTickEnable();
-
 }
 
 int main(void){
     setup(); //Llamar al setup
+    //loop principal
+    while(1) {
+        if(pressed()) {//recibe true, low del botn
+            delay(); // delay para debounce
+            while(!pressed()) {} // Esperar hasta que se suelte el botón para ejecutar, boton no est presionado
 
+            // Ejecutar la secuencia completa al presionar el botón
+            // Verde
+            GPIOPinWrite(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED, GREEN_LED);
+            delay();
 
-//loop principal
-while(1) {
+            // Parpadeante verde
+            GPIOPinWrite(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED, 0);//apaga
+            delay();
+            GPIOPinWrite(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED, GREEN_LED);//enciende
+            delay();
+            GPIOPinWrite(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED, 0);//apaga
+            delay();
 
+            // Amarillo
+            GPIOPinWrite(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED, RED_LED | GREEN_LED);
+            delay();
 
-    //empezar a encender los leds
+            // Rojo
+            GPIOPinWrite(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED, RED_LED);
+            delay();
 
-    //enciende led rojo
-    //gpioporff es para el puerto f
-    //pines quev voy a usar
-
-
-    //pin a enceder, en esre caso el rojos
-
-    GPIOPinWrite(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED, RED_LED);
-    delay();
-
-    // amariilo, es la combinacion enr verde  y rojo
-    GPIOPinWrite(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED, RED_LED | GREEN_LED);
-    delay();
-
-
-    // verde
-    GPIOPinWrite(GPIO_PORTF_BASE, RED_LED | BLUE_LED | GREEN_LED, GREEN_LED);
-    delay();
-
-
+            // Espera hasta el próximo botón presionado
+            while(!pressed()) {}//si el boton no esta presionado sigue sigue ejecutando el bucle
+        }
+    }
 }
-
-
-
-}
-
 
